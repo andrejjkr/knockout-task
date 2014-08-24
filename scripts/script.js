fd0d717e0e6aef080ae55948ea7fac267d95ff11
andrejjkr <andrejj.akr_m@yahoo.com>
2014-08-24 23:19:57 +0300

    фин.

--- scripts/script.js
+++ scripts/script.js
@@ -8,20 +8,20 @@ var documentsViewModel = function(){
         {name:'Описание разработанного инструмента CLM2CRM и его интеграция с salesforce.com (русский).'},
         {name:'Описание разработанного инструмента CLM2CRM и его интеграция с salesforce.com (english).'}
     ];
+
     self.mail = ko.observable();
     self.subject = ko.observable();
     self.mailText = ko.observable();
     self.selectedDocs = ko.observableArray([]);
-
 }
+
 var docVM = new documentsViewModel();
 ko.applyBindings(docVM);
+
 window.displayResults = function(){
     var mail = docVM.mail();
     var subject = docVM.subject();
     var mailText = docVM.mailText();
     var selectedDocsName = docVM.selectedDocs().join('\n');
-    var alertWindow =confirm('E-mail: '+mail+'\n'+'Тема: '+subject+'\n'+'Текст сообщения: '+mailText+'\n'+'Прикрепленные файлы: ' +'\n'+ selectedDocsName);
-
-    alert(alertWindow);
+    alert('E-mail: ' + mail + '\n' + 'Тема: '+subject+'\n'+'Текст сообщения: '+mail+'\n'+'Прикрепленные файлы: ' +'\n'+ selectedDocsName);
 };
